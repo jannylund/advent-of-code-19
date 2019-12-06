60ee38a818d500091c16a1ea050e592ed4cc9bec
Jan Nylund <jan.nylund@mac.com>
2019-12-06 20:53:30 +0200

    Added day06 part 1

---
+++ day06.py
@@ -0,0 +1,46 @@
+from timeit import default_timer as timer
+
+from utils.time import get_time
+
+
+# simply a two dimensional array of who is parent of who.
+def get_parent_tree(m):
+    t = dict()
+    for o in m:
+        p, c = o.split(")")
+        t[c] = p
+    return t
+
+
+def get_parent(t, o):
+    if o in t:
+        return t[o]
+    return None
+
+
+def get_parents(t, o):
+    result = []
+    while True:
+        p = get_parent(t, o)
+        if p is None:
+            return result
+        else:
+            result.append(p)
+        o = p
+    return result
+
+
+def calc_orbits(t):
+    orbits = 0
+    for o in t.keys():
+        orbits += len(get_parents(t, o))
+    return orbits
+
+
+if __name__ == "__main__":
+    with open('input/day06.txt') as f:
+        m = f.read().splitlines()
+    t = get_parent_tree(m)
+    start = timer()
+    print("result day 06 part 1: {} in {}".format(
+        calc_orbits(t), get_time(start)))
---
+++ tests/test_day06.py
@@ -0,0 +1,17 @@
+from day06 import *
+
+
+def test_day06_part1():
+    m = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L".split("\n")
+
+    t = get_parent_tree(m)
+
+    assert get_parent(t, "B") == "COM"
+    assert get_parent(t, "COM") == None
+    assert get_parent(t, "F") == "E"
+
+    assert get_parents(t, "COM") == []
+    assert get_parents(t, "B") == ["COM"]
+    assert get_parents(t, "H") == ["G", "B", "COM"]
+
+    assert calc_orbits(t) == 42
